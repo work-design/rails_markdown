9a0722c99374747a8087e411c1cd2bca4721f1c9
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-28 18:21:56 +0800

    support marp css

--- app/views/markdown/posts/ppt.js
+++ app/views/markdown/posts/ppt.js
@@ -18,9 +18,17 @@ class MarpController extends Controller {
       return response.json()
     }).then(body => {
       const { html, css, comments } = this.marp.render(body.results.markdown)
-      console.log(css)
+      this.installCss(css)
       this.element.innerHTML = html
     })
+  }
+
+  installCss(css) {
+    const element = document.createElement('style')
+    element.type = 'text/css'
+    element.id = 'marp_style'
+    element.textContent = css
+    document.head.insertBefore(element, document.head.lastChild)
   }
 
   get marp() {
