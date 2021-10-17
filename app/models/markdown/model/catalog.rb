6358382ee2d0a0ba7009b5309ccc46be00c8e9f8
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-17 21:22:28 +0800

    catelog

--- app/models/markdown/model/catalog.rb
+++ app/models/markdown/model/catalog.rb
@@ -10,7 +10,7 @@ module Markdown
       belongs_to :parent, foreign_key: :parent_path, primary_key: :path, optional: true
       belongs_to :git
 
-      has_many :posts, foreign_key: :catalog_path, primary_key: :path
+      has_many :posts, ->(o){ where(git_id: o.git_id) }, foreign_key: :catalog_path, primary_key: :path
 
       before_validation :sync_parent_path, if: -> { path_changed? }
     end
