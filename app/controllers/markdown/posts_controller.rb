ee84c3b27ce1103f08a6497d5eac432579095fc4
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-23 12:58:06 +0800

    use find by

--- app/controllers/markdown/posts_controller.rb
+++ app/controllers/markdown/posts_controller.rb
@@ -19,7 +19,7 @@ module Markdown
     def asset
       path = params[:path].match(/assets\/.+/).to_s
       file = "#{path}.#{params[:format]}"
-      asset = Asset.find(path: file)
+      asset = Asset.find_by(path: file)
 
       redirect_to asset.file.url
     end
