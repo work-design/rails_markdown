df1d22b4abc05de226db34727fab47e47b325f7a
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-16 01:48:21 +0800

    files

--- app/controllers/markdown/assets_controller.rb
+++ app/controllers/markdown/assets_controller.rb
@@ -2,8 +2,7 @@ module Markdown
   class AssetsController < BaseController
 
     def asset
-      path = params[:path].match(/assets\/.+/).to_s
-      file = "#{path}.#{params[:format]}"
+      file = "assets/#{params[:path]}.#{params[:format]}"
       asset = Asset.find_by(path: file)
 
       redirect_to asset.file.url, allow_other_host: true
