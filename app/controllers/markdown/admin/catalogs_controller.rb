d9a80b23c18a1a32713716c4ca805d6227af9e62
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-16 20:41:51 +0800

    use shared mdehthods

--- app/controllers/markdown/admin/catalogs_controller.rb
+++ app/controllers/markdown/admin/catalogs_controller.rb
@@ -12,20 +12,6 @@ module Markdown
       @catalogs = @git.catalogs.order(id: :desc)
     end
 
-    def reorder
-      sort_array = params[:sort_array].select { |i| i.integer? }
-
-      if params[:new_index] > params[:old_index]
-        prev_one = @catalog.class.find(sort_array[params[:new_index].to_i - 1])
-        @catalog.insert_at prev_one.position
-      else
-        next_ones = @catalog.class.find(sort_array[(params[:new_index].to_i + 1)..params[:old_index].to_i])
-        next_ones.each do |next_one|
-          next_one.insert_at @catalog.position
-        end
-      end
-    end
-
     private
     def set_git
       @git = Git.find params[:git_id]
